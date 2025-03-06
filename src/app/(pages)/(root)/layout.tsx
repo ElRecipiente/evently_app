7b7f31935937d4a08f31c5cff017a57fd66792c8
Nicolas Droux <nicolas.droux@ecedi.fr>
2025-03-06 12:55:12 +0100

    Ref #2 - Fix tailwind

--- src/app/(pages)/(root)/layout.tsx
+++ src/app/(pages)/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from "next";
-import "../globals.css";
+import "../../globals.css";
 import React from "react";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
