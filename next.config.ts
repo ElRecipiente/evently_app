3ceacef6b269474dc2f0a8c81198d6b4331e2f2f
Nicolas Droux <nicolas.droux@ecedi.fr>
2025-03-07 13:34:19 +0100

    Ref #4 - Add tests

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: false /* Remove the Next helper indicator (N) */
 };
 
 export default nextConfig;
---
+++ tests/example.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test.describe('Responsive Visual Regression', () => {
+  const viewports = [
+    { width: 1920, height: 1080, name: 'desktop' },
+    { width: 1366, height: 768, name: 'laptop' },
+    { width: 768, height: 1024, name: 'tablet' },
+    { width: 375, height: 667, name: 'mobile' },
+  ];
+
+  for (const viewport of viewports) {
+    test(`Test visuel sur ${viewport.name}`, async ({ page }) => {
+      await page.setViewportSize({ width: viewport.width, height: viewport.height });
+      await page.goto('http://localhost:3000/home');
+
+      // Capture et vérifie le screenshot
+      await expect(page).toHaveScreenshot(`screenshot-${viewport.name}.png`);
+    });
+  }
+});
